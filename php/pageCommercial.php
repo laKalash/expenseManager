35a59ad04a27633355f5e1d8535a92f561ffa589
Lucas Brocard Maison <lucasbrocard@gmail.com>
2019-06-21 18:30:10 +0200

    Ajout de la page acceuil commercial et gros avancement de celle-ci

--- php/pageCommercial.php
+++ php/pageCommercial.php
@@ -2,6 +2,7 @@
     require_once 'functions/autoload.php'
 ?>
 
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -9,11 +10,65 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/pageCommercial.css">
     <title>Acceuil-Expense Manager</title>
 </head>
 <body>
+<header><a href="#"><img src="../images/Hamburger_icon.svg.png"></a></header>
 
-<div><input type="search" placeholder="Rechercher"></div>
+<a href="#" id="ajouter"></a>
+
+<input type="search" placeholder="Rechercher" id="recherche">
+
+<div class="container" id="espace">
+    <div class="row">
+        <a href="connection.php" class="col-lg-12 liens">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 4</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 5</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 6</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 7</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 8</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 9</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 10</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        <a href="#" class="col-lg-12 liens">
+            <h3>Column 11</h3>
+            <p>Lorem ipsum dolor..</p>
+        </a>
+        
+    </div>
+</div>
 
 </body>
 </html>
